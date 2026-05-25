60c43b26b2587a3b17acb391b5d3f1d365502482
psatomas <psatomas@gmail.com>
2026-05-25 20:52:01 -0300

    feat: implement ownership authorization and access control

--- scripts/interact.ts
+++ scripts/interact.ts
@@ -34,7 +34,7 @@ async function main() {
     // =========================================================
 
     const contractAddress =
-        "YOUR_DEPLOYED_CONTRACT_ADDRESS";
+        "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
     // =========================================================
     //                    OWNER CONTRACT
